a08578326a1adc96c51e6b77489598a986aef12b
zam-dev <zamsyh.work@gmail.com>
2022-03-15 14:51:16 +0700

    Implement 404 not found

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Article from "./pages/Article";
 import ArticlesList from "./pages/ArticlesList";
+import NotFound from "./pages/errors/404"
 
 //Components
 import Navbar from "./components/Navbar";
@@ -16,6 +17,7 @@ function App() {
       <Navbar />
       <div className='max-w-screen-md pt-16 mx-auto'>
         <Routes>
+          <Route Route path="*" element={<NotFound/>}></Route>
           <Route exact path="/" element={<Home/>}></Route>
           <Route exact path='/about' element={<About/>}></Route>
           <Route exact path='/articles-list' element={<ArticlesList />}></Route>
---
+++ src/pages/errors/404.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <>
+      <div class="bg-base-200">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">404 - Page Not Found</h1>
+                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default NotFound
