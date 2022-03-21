da415d55c55dcf1460fcf36a914a4c523d6c3298
zam-dev <zamsyh.work@gmail.com>
2022-03-21 19:01:10 +0700

    Create validation models articles

--- backend/models/articles.js
+++ backend/models/articles.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = require('mongoose');
 
 const articleSchema = new Schema({
-    name: String,
-    title: String,
-    thumbnail: String,
-    content: String
+    name: { type: String, min: [6, 'Name to short!'], required: [true, 'Name is required!'] },
+    title: { type: String, required: [true, 'Title is required!']},
+    thumbnail:{ type: String, required: [true, 'Thumbnail is required!']},
+    content: { type: String, required: [true, 'Content is required!']}
 });
 
 
