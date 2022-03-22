2e610996d9051e7ed50073110cade7b759034c5c
zam-dev <zamsyh.work@gmail.com>
2022-03-22 11:43:53 +0700

    Create users models

---
+++ backend/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const userModel = new Schema({
+    name: { 
+        type: String, 
+        min:[4, "Min 6 characters"], 
+        required:[true, "Name required"] 
+    },
+    email: {
+        type: String,
+        required: [true, "Email required"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password required!"]
+    }
+
+})
+
+module.exports = mongoose.model('user', userModel);
